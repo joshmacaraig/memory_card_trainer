b0cec569220c749c1a6f9a604ebc1d61790790ff
josh <bajejosh@gmail.com>
2023-11-20 03:28:34 +0800

    Update MetronomeControl.jsx

--- src/components/MetronomeControl.jsx
+++ src/components/MetronomeControl.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import ImageDisplay from './ImageDisplay';
+import './MetronomeControl.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons';
-import './MetronomeControl.css';
 
 const MetronomeControl = () => {
     const [isRunning, setIsRunning] = useState(false);
@@ -12,39 +12,37 @@ const MetronomeControl = () => {
     const [seconds, setSeconds] = useState(1);
     const [currentIndex, setCurrentIndex] = useState(0);
     const [images, setImages] = useState([]);
-    const [imagesLoaded, setImagesLoaded] = useState(false);
-    const [loadedImageCount, setLoadedImageCount] = useState(0);
-    const [isMuted, setIsMuted] = useState(false);
+    const [showImage, setShowImage] = useState(false); // State to control image visibility
     const totalCards = 52;
     const beatSound = new Audio('/sounds/metronome-85688.mp3');
-    const [showImage, setShowImage] = useState(false);
+    const [isMuted, setIsMuted] = useState(false);
+    const [allImagesLoaded, setAllImagesLoaded] = useState(false);
+    const [loadedImageCount, setLoadedImageCount] = useState(0);
 
     useEffect(() => {
+        // Generate and shuffle card images
         const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
         const values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
-
         let newImages = suits.flatMap(suit => 
-            values.map(value => `/images/${value}_of_${suit}.png`)
+            values.map(value => `${value}_of_${suit}.png`)
         );
 
         newImages.sort(() => Math.random() - 0.5);
 
-        newImages.forEach(imageSrc => {
+        // Load all images
+        newImages.forEach((image, index) => {
             const img = new Image();
-            img.src = imageSrc;
+            img.src = `/images/${image}`;
             img.onload = () => {
-                setLoadedImageCount(prevCount => prevCount + 1);
+                setLoadedImageCount(count => count + 1);
+                if (index === newImages.length - 1) {
+                    setAllImagesLoaded(true);
+                }
             };
         });
 
         setImages(newImages);
     }, []);
-
-    useEffect(() => {
-        if (loadedImageCount === totalCards) {
-            setImagesLoaded(true);
-        }
-    }, [loadedImageCount]);
 
     useEffect(() => {
         if (isPreparing) {
@@ -72,6 +70,7 @@ const MetronomeControl = () => {
                         return newIndex;
                     } else {
                         setIsRunning(false);
+                        setShowImage(false);
                         return prevIndex;
                     }
                 });
@@ -80,65 +79,72 @@ const MetronomeControl = () => {
         return () => clearInterval(interval);
     }, [isRunning, seconds, cardsToShow, currentIndex, isMuted]);
 
+    const toggleMute = () => {
+        setIsMuted(!isMuted);
+    };
+
+
     const handleStart = () => {
         setIsPreparing(true);
     };
 
-    const toggleMute = () => {
-        setIsMuted(!isMuted);
+    const handleReset = () => {
+        setCurrentIndex(0);
+        setIsRunning(false);
+        setShowImage(false);
+        setCountdown(3);
     };
 
     const totalTime = (totalCards / cardsToShow * seconds).toFixed(2);
 
     return (
         <div className="metronome-control">
-            {!imagesLoaded && (
-                <div>Loading images... {Math.round((loadedImageCount / totalCards) * 100)}%</div>
-            )}
-
-            {imagesLoaded && (
-                <>
-                    {isPreparing ? (
-                        <div className="countdown">{countdown}</div>
-                    ) : (
-                        !isRunning && (
-                            <button className="start-button" onClick={handleStart}>
-                                Start
-                            </button>
-                        )
-                    )}
-                    {showImage && <ImageDisplay image={images[currentIndex]} />}
-                </>
-            )}
-
             <div onClick={toggleMute} className="mute-icon">
-                <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} />
+                {isMuted ? 
+                    <FontAwesomeIcon icon={faVolumeMute} /> : 
+                    <FontAwesomeIcon icon={faVolumeUp} />
+                }
             </div>
-
-            <div className="controls">
-                <button onClick={handleStart}>Reset</button>
-                <div>
-                    <label>Cards: </label>
-                    <input 
-                        type="number" 
-                        min="1" 
-                        max="52" 
-                        value={cardsToShow} 
-                        onChange={(e) => setCardsToShow(parseInt(e.target.value, 10))} 
-                    />
-                    <label> Seconds: </label>
-                    <input 
-                        type="number" 
-                        min="1" 
-                        max="60" 
-                        value={seconds} 
-                        onChange={(e) => setSeconds(parseInt(e.target.value, 10))} 
-                    />
-                    <span> - Total Time: {totalTime} seconds</span>
-                </div>
+            <div className='main-content'>
+                {!allImagesLoaded && (
+                    <div>Loading images... {Math.round((loadedImageCount / totalCards) * 100)}%</div>
+                )}
+    
+                {allImagesLoaded && (
+                    <>
+                        {isPreparing ? 
+                            <div className="countdown">{countdown}</div> : 
+                            (!isRunning && <button className="start-button" onClick={handleStart}>Start</button>)
+                        }
+                        {showImage && <ImageDisplay image={images[currentIndex]} />}
+                        <div className="controls">
+                            <button onClick={handleReset}>Reset</button>
+                            <div>
+                                <label>Cards: </label>
+                                <input 
+                                    type="number" 
+                                    min="1" 
+                                    max="52" 
+                                    value={cardsToShow} 
+                                    onChange={(e) => setCardsToShow(parseInt(e.target.value, 10))} 
+                                />
+                                <label> Seconds: </label>
+                                <input 
+                                    type="number" 
+                                    min="1" 
+                                    max="60" 
+                                    value={seconds} 
+                                    onChange={(e) => setSeconds(parseInt(e.target.value, 10))} 
+                                />
+                            </div>
+                            <p>Total Time: {totalTime} seconds</p>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
+    
 };
 
 export default MetronomeControl;
